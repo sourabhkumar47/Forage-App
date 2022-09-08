9c2d237b7e695ff7b39197894e1cd23aa74b2498
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-08 16:56:53 +0530

    implemented Add Fragment

--- app/src/main/java/com/example/forage/ui/AddForageableFragment.kt
+++ app/src/main/java/com/example/forage/ui/AddForageableFragment.kt
@@ -74,6 +74,12 @@ class AddForageableFragment : Fragment() {
 
             // TODO: Observe a Forageable that is retrieved by id, set the forageable variable,
             //  and call the bindForageable method
+            viewModel.retrieveForageable(id).observe(this.viewLifecycleOwner){selectedForageable ->
+                forageable = selectedForageable
+                bindForageable(forageable)
+
+
+            }
 
             binding.deleteBtn.visibility = View.VISIBLE
             binding.deleteBtn.setOnClickListener {
